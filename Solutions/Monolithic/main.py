bdc70a2e76d3954233a6b55eb5170f47f4b5886e
HaidarJbeily7 <h.jbeily@innopolis.university>
2024-12-04 23:28:08 +0300

    fix bug

--- Solutions/Monolithic/main.py
+++ Solutions/Monolithic/main.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
         p_publish.start()
 
         logger.info("Starting Flask application")
-        app.run(host='0.0.0.0', port=5001)
+        app.run(host='0.0.0.0', port=5000)
 
         p_filter.join()
         p_screaming.join()
